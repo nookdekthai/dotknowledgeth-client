9d92d7c83c9aaee6d602371efac6676ee6b89975
21 Tech Enterprise <21techenterprise@Macintosh.local>
2023-10-16 22:00:17 +0700

    done

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -96,7 +96,7 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
                       href={"/"}
                       className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}
                     >
-                      Forex Course
+                      Dotknowledgeth
                     </Link>
                   </div>
                   <div className="flex items-center">
