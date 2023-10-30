952bba3389b83eb96efadd463f19e34090e5b25a
21 Tech Enterprise <>
2023-10-30 23:51:48 +0700

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
 
 import Home from "./components/HomeNew/Home";
+export const metadata = {
+  title: 'Studying English: Online Course สอนภาษาอังกฤษ สำหรับทุกคน',
+  description: 'สอนภาษาอังกฤษวัยเด็ก วัยทำงาน ปูพื้นฐาน ติวสอบทุกสนาม TOEIC ราชการ กพ เข้ามหาวิทยาลัย มีทั้งคลาสสด และออนไลน์. ผู้สอนมีประสบการณ์. ปูพื้นฐานนักเรียนไม่มีพื้นฐานให้เก่งได้ ',
+  openGraph: {
+    title: 'Studying English: Online Course สอนภาษาอังกฤษ สำหรับทุกคน',
+    description: 'สอนภาษาอังกฤษวัยเด็ก วัยทำงาน ปูพื้นฐาน ติวสอบทุกสนาม TOEIC ราชการ กพ เข้ามหาวิทยาลัย มีทั้งคลาสสด และออนไลน์. ผู้สอนมีประสบการณ์. ปูพื้นฐานนักเรียนไม่มีพื้นฐานให้เก่งได้',
+    url: 'https://dotknowledgeth.vercel.app',
+    siteName: 'dotknowledgeth',
+    images: [
+      {
+        url: 'https://dotknowledgeth.com/wp-content/uploads/2020/06/97496214_261523775036041_1655514634804789248_n.jpg',
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'th-TH',
+    type: 'website',
+  },
+  twitter: {
+    card: 'Studying English: Online Course สอนภาษาอังกฤษ สำหรับทุกคน',
+    title: 'สอนภาษาอังกฤษวัยเด็ก วัยทำงาน ปูพื้นฐาน ติวสอบทุกสนาม TOEIC ราชการ กพ เข้ามหาวิทยาลัย มีทั้งคลาสสด และออนไลน์. ผู้สอนมีประสบการณ์. ปูพื้นฐานนักเรียนไม่มีพื้นฐานให้เก่งได้',
+    description: 'สอนภาษาอังกฤษวัยเด็ก วัยทำงาน ปูพื้นฐาน ติวสอบทุกสนาม TOEIC ราชการ กพ เข้ามหาวิทยาลัย มีทั้งคลาสสด และออนไลน์. ผู้สอนมีประสบการณ์. ปูพื้นฐานนักเรียนไม่มีพื้นฐานให้เก่งได้ ',
+    images: ['https://dotknowledgeth.com/wp-content/uploads/2020/06/97496214_261523775036041_1655514634804789248_n.jpg'],
+  },
+}
+
 
 export const revalidate = 180
 
@@ -12,6 +38,7 @@ const Page = async () => {
   const [resBanner, resCategory] = await Promise.all([pmBanner, pmCategory])
   const banner = await resBanner.json()
   const category = await resCategory.json()
+  
 
 
   const webInfo = {
