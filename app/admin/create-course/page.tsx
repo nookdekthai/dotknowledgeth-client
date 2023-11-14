f84e098778ecf5a64d6da29e61fc52f8f823e407
Teerapong Sitthikarn <git-ssdd-user@truecorp.co.th>
2023-11-14 21:14:46 +0700

    fix open course

--- app/admin/create-course/page.tsx
+++ app/admin/create-course/page.tsx
@@ -1,35 +1,29 @@
 'use client'
-import DashboardHero from '@/app/components/Admin/DashboardHero'
-import AdminProtected from '@/app/hooks/adminProtected'
-import Heading from '@/app/utils/Heading'
 import React from 'react'
 import AdminSidebar from "../../components/Admin/sidebar/AdminSidebar";
-import AllCourses from "../../components/Admin/Course/AllCourses";
-import BlogInformation from '@/app/components/Admin/Blog/BlogInformation'
-// import AllEbook from '@/app/components/Admin/ebook/AllEbook'
+import Heading from '../../../app/utils/Heading';
+import CreateCourse from "../../components/Admin/Course/CreateCourse";
+import DashboardHeader from '../../../app/components/Admin/DashboardHeader';
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
     <div>
-      <AdminProtected>
         <Heading
-          title="Elearning - Admin"
-          description="ELearning is a platform for students to learn and get help from teachers"
-          keywords="Programming,MERN,Redux,Machine Learning"
+         title="Elearning - Admin"
+         description="ELearning is a platform for students to learn and get help from teachers"
+         keywords="Prograaming,MERN,Redux,Machine Learning"
         />
-        <div className="flex min-h-screen">
-          <div className="1500px:w-[16%] w-1/5">
-            <AdminSidebar />
-          </div>
-          <div className="w-[85%]">
-            <DashboardHero />
-            <BlogInformation />
-            {/* <AllEbook /> */}
-          </div>
+        <div className="flex">
+            <div className="1500px:w-[16%] w-1/5">
+                <AdminSidebar />
+            </div>
+            <div className="w-[85%]">
+               <DashboardHeader />
+               <CreateCourse /> 
+            </div>
         </div>
-      </AdminProtected>
     </div>
   )
 }
